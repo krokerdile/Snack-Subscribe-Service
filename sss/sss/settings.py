7cd7d062bbce402102824b08660413f4f8bf790a
hyeonjin <ki1029m@naver.com>
2020-10-17 17:21:38 +0900

    수정

--- sss/sss/settings.py
+++ sss/sss/settings.py
@@ -126,3 +126,6 @@ STATICFILES_DIRS = [
     # os.path.join(BASE_DIR, 'account', 'static'),
     # os.path.join(BASE_DIR, 'main','static'),
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
