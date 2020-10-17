30bb6fda3de3d17ab55abbe8c8bab4e10f938b02
hyeonjin <ki1029m@naver.com>
2020-10-17 20:14:37 +0900

    과자모델추가

--- sss/main/models.py
+++ sss/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Snack(models.Model):
+    name=models.CharField(max_length=50, null=False)
+    describe=models.CharField(max_length=500)
+    summary =models.CharField(max_length=300)
+
+    image=models.ImageField(upload_to='images/', null=True)
+
+    with_who =models.CharField(max_length=20)
+    texture =models.CharField(max_length=20)
+    flavor =models.CharField(max_length=20)
+    situation =models.CharField(max_length=20)
