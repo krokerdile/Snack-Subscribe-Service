115713ce053b262ba491f30fd39ba287116f8fc9
hyeonjin <ki1029m@naver.com>
2020-10-17 16:33:52 +0900

    새로운 템플릿 적용

--- sss/main/views.py
+++ sss/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    return render(request,'index.html')
 
 def recommend(request):
     return render(request,'recommend.html')
